b63d0daffad239a43e91cb9f8ddd155d5900f0bc
Jordan S Read <jread@usgs.gov>
2014-04-25 08:48:36 -0500

    fleshing out basic station functions

---
+++ rGLTC/R/CRU.station.R
@@ -0,0 +1,9 @@
+find.stations.grid <- function(lat,long){
+  # finds station IDs within a grid point
+  
+}
+
+get.station.data <- function(station.id){
+  #finds station data, converts into data.frame.
+  
+}
