8dee4a82c125c7149421c0afd655df0907cf48a0
Jordan S Read <jread@usgs.gov>
2014-07-12 14:49:32 -0500

    fix for fail plot issue w/ cleanair pkg

--- rGLTC/R/write.CC.sen.all.R
+++ rGLTC/R/write.CC.sen.all.R
@@ -11,9 +11,8 @@ lon.vals	<<-	ncvar_get(nc,varid="longitude")
 
 years <- seq(1985,2009)
 
-get.all.CC <-	function(years,period='JAS'){
-  vals.out <- rep( list(list()), length(years) )
-  names(vals.out) <- paste('yr',years,sep='')
+get.all.CC <-	function(years,period='JAS',lat.i,lon.i){
+  vals.out <- rep( NA, length(years) )
   require(ncdf4)
   if (period=="JAS"){
     mm.idx  <-	c(7,8,9)
@@ -25,32 +24,27 @@ get.all.CC <-	function(years,period='JAS'){
   for (i in 1:length(years)){
     year <- years[i]
     nc  <-	nc_open(filename=paste(data.dir,'CA_PATMOSX_NOAA_0130PM_',as.character(year),'.nc',sep=''))
-    CA_1	<-	ncvar_get(nc=nc, varid="a_CA",start=c(1,1,mm.idx[1]),count=c(length(lon.vals),length(lat.vals),1)) # hack for mean of JAS
-    CA_2  <-	ncvar_get(nc=nc, varid="a_CA",start=c(1,1,mm.idx[2]),count=c(length(lon.vals),length(lat.vals),1))
-    CA_3  <-	ncvar_get(nc=nc, varid="a_CA",start=c(1,1,mm.idx[3]),count=c(length(lon.vals),length(lat.vals),1))
-    year_CA = (CA_1+CA_2+CA_3)/3
+    CA	<-	ncvar_get(nc=nc, varid="a_CA",start=c(lon.i,lat.i,mm.idx[1]),count=c(1,1,length(mm.idx)))
     nc_close(nc)
-    vals.out[[i]] <- year_CA
+    vals.out[i] <- mean(CA)
   }
-  
-  
   return(vals.out)
 }
 
-vals.out <- get.all.CC(years)
-#source("gap.match.R")
-sens <- matrix(data = NA,nrow = length(lon.vals),ncol=length(lat.vals))
 
-for (j in 1:length(lat.vals)){
+#source("gap.match.R")
+summaryTxt = paste('/Users/jread/Documents/GLTC-stats/rGLTC/data/CC_all_sen.csv',sep='')
+cat("lat,lon,slope(CC)\n", file=summaryTxt,append=FALSE)
+
+
+
+for (j in 19:length(lat.vals)){
   for (i in 1:length(lon.vals)){
-    vals <- vector(mode = 'numeric',length=length(vals.out))
-    for (k in 1:length(vals.out)){
-      vals[k] <- vals.out[[k]][i, j] # note i and j switched...
-      cat(i);cat(j);cat(k);cat('\n')
-    }
-    temp.df <- data.frame("Temp"=vals,"date"=as.Date(paste(years,'-1-1',sep='')))
-    cat(i);cat(j);cat('\n')
-    sens[i,j] <- calc.sen.slope(temp.df)$slope
+
+    vals.out <- get.all.CC(years,period='JAS',lat.i=j,lon.i=i)
+    temp.df <- data.frame("Temp"=vals.out,"date"=as.Date(paste(years,'-1-1',sep='')))
+    sens <- calc.sen.slope(temp.df)$slope
+    cat(paste(c(lat.vals[j],',',lon.vals[i],',',sens,'\n'),collapse=''), file=summaryTxt,append=TRUE)
   }
-  cat(j);cat(' of ');cat(length(lat.vals));cat('\n')
+  cat(j);cat(' of ');cat(length(lat.vals));cat('done......\n')
 }
