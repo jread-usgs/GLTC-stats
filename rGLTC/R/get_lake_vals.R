a05932c2fa357e69e8b7369ca1b606a510866f1f
Jordan S Read <jread@usgs.gov>
2014-08-22 13:55:07 -0500

    other regions as single blobs

--- rGLTC/R/get_lake_vals.R
+++ rGLTC/R/get_lake_vals.R
@@ -16,7 +16,7 @@ get_time <- function(line, delim = ','){
   return(time)
 }
 
-build_lake_list <- function(time, data, sat_time, sat_data, lake_names, lat, lon){
+build_lake_list <- function(time, data, sat_time, sat_data, lake_names, lat, lon, region){
   
   lake_data = vector("list", length(lake_names))
   names(lake_data) <- lake_names
@@ -33,19 +33,32 @@ build_lake_list <- function(time, data, sat_time, sat_data, lake_names, lat, lon
       if (any(u_i)){ # else it stays empty
         time_sub <- sat_time[u_i]
         temp_sub <- temperatures[u_i]
-        lake_data[[i]] <- list('lat'=lat[i],'lon'=lon[i],'time'=time_sub,'temperature'=temp_sub)
+        lake_data[[i]] <- list('lat'=lat[i],'lon'=lon[i],'time'=time_sub,'temperature'=temp_sub, 'region' =region[i])
       }
     } else {
-      lake_data[[i]] <- list('lat'=lat[i],'lon'=lon[i],'time'=time_sub,'temperature'=temp_sub)
+      lake_data[[i]] <- list('lat'=lat[i],'lon'=lon[i],'time'=time_sub,'temperature'=temp_sub, 'region' =region[i])
     }
     
   }
   return(lake_data)
 }
 
+get_region_matches <- function(lake_data, regions){
+  
+  u_i <- vector(length = length(lake_data))
+  for (i in 1:length(lake_data)){
+    if (any(regions %in% lake_data[[i]]$region)){
+      u_i[i] = TRUE
+    }
+  }
+  region_matches <- names(lake_data)[u_i]
+  return(region_matches)
+}
+
 # open master
 library("stringr")
-min_size <<- 0.5
+regions <- c("ENA", "WNA", "SENA")
+min_size <<- 1
 delim = ','
 con <- file('../data/Master_2014-06-03.csv', "r", blocking = FALSE)
 time_rng <- c(1985,2009)
@@ -53,7 +66,9 @@ p_val <- 0.01
 min_n <- 13
 line <- readLines(con, n = 1)
 lake_names <- str_split(line, delim)[[1]][-2:-1]
-line <- readLines(con, n = 4) # code, etc
+line <- readLines(con, n = 3) # code, etc
+line <- readLines(con, n = 1)
+region <- str_split(line, delim)[[1]][-2:-1]
 line <- readLines(con, n = 1)
 lat <- as.numeric(str_split(line, delim)[[1]][-2:-1])
 line <- readLines(con, n = 1)
@@ -72,7 +87,9 @@ sat_data <- get_data(line, ncol = length(lake_names))
 sat_time <- get_time(line)
 
 # lake_data is the list of the lat, lon, time and temp for each lake.
-lake_data <- build_lake_list(situ_time, situ_data, sat_time, sat_data, lake_names, lat, lon)
+lake_data <- build_lake_list(situ_time, situ_data, sat_time, sat_data, lake_names, lat, lon, region)
+plot_lakes <- get_region_matches(lake_data, regions)
+
 library(Hmisc)
 
 library(pracma)
@@ -98,17 +115,28 @@ match_cor <- function(lake_1,lake_2, detrend = TRUE){
     p<- data.frame(p=NA, r =  NA)
   }
   
-  
   return(p)
+}
+get_lat_region <- function(lake){
+  lat <- data.frame('Africa' = 32, 'Asia' = 44, 'Oceania' = 19, 
+                    'Europe' = 58, 'Middle.East' = 39, 'South.America' = 21)
+  
+  lon <- data.frame('Africa' = -166, 'Asia' = -170, 'Oceania' = -150, 
+                    'Europe' = -55, 'Middle.East' = -55, 'South.America' = -82)
+  
+  region <- strrep(lake$region, " ", ".")
+  lake$lat <- as.numeric(lat[region])
+  lake$lon <- as.numeric(lon[region][1])
+  return(lake)
 }
 
 library(maps)
 library(mapdata)
 library(geosphere)
-xlim <- c(-171.738281, -56.601563)
-ylim <- c(12.039321, 71.856229)
+xlim <- c(-171.738281, -52.601563)
+ylim <- c(18.039321, 75.856229)
 
-map("world", xlim=xlim, ylim=ylim) #col="grey80", fill=TRUE, bg="white", lwd=0.05, 
+map("worldHires",c("Canada","USA", "Mexico"), xlim=xlim, ylim=ylim) #col="grey80", fill=TRUE, bg="white", lwd=0.05, 
 node_size <- function(p_vals){
   sig <- sum(p_vals < p_val, na.rm = T)
   sz <- min_size+sig/100
@@ -117,21 +145,30 @@ node_size <- function(p_vals){
 
 add_arc <- function(lake_1,lake_2,r_val){
   inter <- gcIntermediate(c(lake_1$lon, lake_1$lat), c(lake_2$lon, lake_2$lat), n=20, addStartEnd=TRUE)
-  lines(inter, lwd = .2, col=rgb(0,0,0,.1*abs(r_val),1))
+  lines(inter, lwd = .2, col=rgb(0,0,0,.3*abs(r_val),1))
 }
-for (k in 1:length(lake_names)){
-  lake_1 <- lake_data[[k]] # lake_2 is all other lakes 
+
+
+for (k in 1:length(plot_lakes)){
+  u_i <- which(names(lake_data) == plot_lakes[k])
+  lake_1 <- lake_data[[u_i]] # lake_2 is all other lakes 
   u_i <- lake_1$time >= time_rng[1] & lake_1$time <= time_rng[2]
   lake_1$time <- lake_1$time[u_i]
   lake_1$temperature <- lake_1$temperature[u_i]
-  other_lakes <- lake_names[lake_names != lake_names[k]]
+  other_lakes <- lake_names[lake_names != plot_lakes[k]]
   p_vals <- vector(length=length(other_lakes))
   for (i in 1:length(other_lakes)){
+    
     lake_2 <- lake_data[[other_lakes[i]]]
+    other_region <- lake_2$region
     cor_v <- match_cor(lake_1,lake_2)
     p_vals[i] <- cor_v$p
     r_val <- cor_v$r
     if (!is.na(p_vals[i]) & p_vals[i] < p_val){
+      
+      if (!lake_2$region %in% regions){
+        lake_2 <- get_lat_region(lake_2)
+      }
       add_arc(lake_1,lake_2,r_val)
     }
     #cat(p_vals[i]); cat('\n')
